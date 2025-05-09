5ea99a0250c1b42b227c74ae337bf012501d0c26
Kostiantyn.Aleksieienko <00gevill00@gmail.com>
2025-05-08 22:20:38 -0700

    + showWinnerModal

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,5 +1,31 @@
 import showModal from './modal';
+import createElement from '../../helpers/domHelper';
 
 export default function showWinnerModal(fighter) {
-    // call showModal function
+    const title = `🏆 Winner: ${fighter.name}`;
+
+    const bodyElement = createElement({
+        tagName: 'div',
+        className: 'modal___body'
+    });
+
+    const fighterImage = createElement({
+        tagName: 'img',
+        className: 'modal___img',
+        attributes: {
+            src: fighter.source,
+            title: fighter.name,
+            alt: fighter.name
+        }
+    });
+    bodyElement.append(fighterImage);
+
+    const fighterName = createElement({
+        tagName: 'h3',
+        className: 'modal___name',
+        innerText: fighter.name
+    });
+    bodyElement.append(fighterName);
+
+    showModal({ title, bodyElement });
 }
